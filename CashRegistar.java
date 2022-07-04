ef5173e4d7d6e8a419d241e2758aafdc5ff6408c
cjjaquith <71270371+cjjaquith@users.noreply.github.com>
2022-07-04 13:01:52 -0400

    Add files via upload

--- CashRegistar.java
+++ CashRegistar.java
@@ -2,56 +2,107 @@
 // Course: CIS357
 // Due Date: 7/5/2022
 // Name: Christian Jaquith
-// GitHub: xxx
+// GitHub: https://github.com/cjjaquith/cis357-hw1-Jaquith
 // Instructor: Il-Hyung Cho
 // Program Description: This program simulates a cash registar at a grocery store.
+import java.util.Locale;
 import java.util.Scanner;
-import java.lang.Math;
-
 /** Javadoc commenets for CashRegistar.java */
 public class CashRegistar {
     public static void main(String[] args) {
-        System.out.println("Enter product code: ");
-
-        int[] itemArray = new int[10];
-        itemArray[0] = 1;
-        itemArray[1] = 2;
-        itemArray[2] = 3;
-        itemArray[3] = 4;
-        itemArray[4] = 5;
-        itemArray[5] = 6;
-        itemArray[6] = 7;
-        itemArray[7] = 8;
-        itemArray[8] = 9;
-        itemArray[9] = 10;
-
         Scanner input = new Scanner(System.in);
-        // Decides what gets printed based off of input from user
-        if (input.nextInt() == itemArray[0]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[1]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[2]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[3]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[4]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[5]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[6]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[7]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[8]) {
-            System.out.println("Enter item name: ");
-        } else if (input.nextInt() == itemArray[9]) {
-            System.out.println("Enter item name: ");
-        } else {
-            getReceipt();
+        String [] data = new String[100];
+        int count = 0;
+        int code;
+        double totalDataCode = 0;
+        double sales = 0;
+
+        System.out.println("Welcome to Jaquiht's cash register system");
+        System.out.println ("");
+        System.out.println("Beginnig a new sale (Y/N) ");
+        String answer = input.next();
+
+        while (answer.toLowerCase(Locale.ROOT).equals("y")) {
+            if (answer.toLowerCase(Locale.ROOT).equals("y")) {
+                do {
+                    Item n1 = new Item();
+
+                    System.out.print("Enter product code: ");
+                    String value1 = input.next();
+
+                    while (value1.charAt(0) < 48 || value1.charAt(0) > 57) {
+                        System.out.println("!!!Invalid product code");
+                        System.out.print("Enter priduct code: ");
+                        value1 = input.next();
+                    }
+                    code = Integer.parseInt(value1);
+
+                    if (code == -1) {
+                        continue;
+                    }
+
+                    System.out.println("       item name: " + n1.getName(code));
+                    String itemName = n1.getName(code);
+                    double price = n1.getPrice(code);
+
+                    System.out.print("Enter quantity: ");
+                    value1 = input.next();
+
+                    while (value1.charAt(0) < 48 || value1.charAt(0) > 57) {
+                        System.out.println("!!!Invalid quantity");
+                        System.out.print("Enter quantity code: ");
+                        value1 = input.next();
+                    }
+                    int quantity = Integer.parseInt(value1);
+
+                    double total = quantity * price;
+                    System.out.println("Item total : $ " + total);
+
+                    totalDataCode = totalDataCode + total;
+                    data[count] = quantity + " " + itemName + " $" + String.format("%.2f", total);
+                    count++;
+                }
+                while (code != -1);
+
+                System.out.println("-----------------------");
+                System.out.println("Item list: ");
+                for (String datum : data) {
+                    if (datum != null) {
+                        System.out.print(datum);
+                    }
+                }
+                System.out.println("SubTotal  $" + totalDataCode);
+                double totalWithTax = totalDataCode * 6 / 100;
+                System.out.println("Total with tax " + String.format("%.2f", totalWithTax));
+                System.out.print("Tendered amount: $ ");
+                double amountPay = Double.parseDouble(input.next());
+
+                while (true) {
+                    if (totalWithTax - amountPay > 0) {
+                        System.out.println("Enter tenderd amount again.");
+                        amountPay = Double.parseDouble(input.next());
+                    } else {
+                        break;
+                    }
+                }
+
+                sales = sales + totalWithTax;
+                double change = amountPay > totalWithTax ? amountPay - totalWithTax : 0;
+                System.out.println("Change: " + String.format("%.2f", change));
+                System.out.println("-----------------------");
+
+                System.out.println();
+                System.out.println();
+
+                System.out.println("-----------------------");
+                System.out.println("Beginning new sale (Y/N) ");
+                answer = input.next();
+            }
         }
-    }
-    public static void getReceipt() {
-        System.out.print("receipt");
+
+        if (answer.toLowerCase(Locale.ROOT).equals("n")) {
+            System.out.println("Total sales of the day is $" + String.format("%.2f", sales));
+            System.out.println("Thanks for using POST system. Goodbye.");
+        }
     }
 }
